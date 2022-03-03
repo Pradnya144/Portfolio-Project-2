d61c98809165b10b1d6ce13602a83a43a2ef4e51
Pradnya Bhagwat <pradnya144@gmail.com>
2022-03-03 17:06:25 +0000

    Add function to generate a random digit from 0 to 9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("testing connection");
+
+/**
+ * Generate a random digit between 0-9
+ */
+
+function randomDigit() {
+
+    var num = Math.floor(Math.random()*10);
+    return num;
+}
+
