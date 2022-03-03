6f56ecf5c40aa31ac23314036ad37140e4a2020f
Pradnya Bhagwat <pradnya144@gmail.com>
2022-03-03 17:20:32 +0000

    Add function to generate 4 digit number

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ function randomDigit() {
     return num;
 }
 
+/**
+ * function to generate a 4 digit number
+ * having distinct digits
+ */
+
+function generateNumber() {
+
+    var num1= randomDigit();
+
+    for(let i; i=1; i++) {
+        var num2 = randomDigit();
+        var num3 = randomDigit();
+        var num4 = randomDigit();
+        if(num3 != num4 && num2 != num4 && num1 != num4 && num2 != num3 && num1 != num2 && num1 != num3) {
+            break;
+        }
+    }
+    var ans = [num1, num2, num3, num4];
+    return ans;
+}
