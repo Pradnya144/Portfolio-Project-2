51603b80a2b4bd11cf9958198f413d9156c8fd62
Pradnya Bhagwat <pradnya144@gmail.com>
2022-03-05 12:08:22 +0000

    Add function to compare users answer and computers number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//global objects
+var attempt = 0;
+
 // Event listeners for buttons
 
 let buttons = document.getElementsByTagName("button");
@@ -47,10 +50,40 @@ function generateNumber() {
     return ans;
 }
 
+ans = generateNumber();
 /**
  * function to check number of bulls and cows
  */
 
  function checkAnswer() {
+
+     var bullcount = 0;
+     var cowcount = 0;
+     var userInput = document.getElementsByClassName("user-input");
+     //check empty input
+     if(userInput[0].value.length < 4) {
+         alert("Please enter a 4 digit number");
+     } 
+     //check repeated digits
+     if((/([0-9]).*?\1/).test(userInput[0].value)) {
+         alert("The digits in the number should not repeat!");
+     } else {
+         for(var n=0; n < 4; n++){
+             for(var i=0; i < 4; i++) {
+                 if(userInput[0].value.charAt(i) === ans[n] && i === n) {
+                     bullcount++;
+                 } else if(userInput[0].value.charAt(i) === ans[n]) {
+                     cowcount++;
+                 }
+             }
+         }
+         attempt++;
+         if(bullcount === 4) {
+             console.log("Congratulations!");
+         } else {
+             document.getElementsByClassName("bulls")[1].innerHTML = bullcount;
+             document.getElementsByClassName("cows")[1].innerHTML = cowcount;
+         }
+     }
 
  }
